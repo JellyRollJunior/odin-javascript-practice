79f573c1f16bef9508e0fcf53d465c10a5be7103
Brandon Lin <brandonlin9716@gmail.com>
2024-09-26 21:42:22 -0700

    hide/show dropdown menu on click dropdown menu button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-import './styles.css'
+import './styles.css';
 
 console.log('hello world');
+
+const dropdownMenuButton = document.querySelector('.dropdown-menu-btn');
+dropdownMenuButton.addEventListener('click', () => {
+  const dropdownMenu = document.querySelector('.dropdown-menu');
+  if (dropdownMenu.classList.contains('visible')) {
+    dropdownMenu.classList.remove('visible');
+  } else {
+    dropdownMenu.classList.add('visible');
+  }
+});
