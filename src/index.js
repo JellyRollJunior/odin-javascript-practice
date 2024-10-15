6869be36cde96959f85ce0021b61d707145523c8
Brandon Lin <brandonlin9716@gmail.com>
2024-10-15 09:22:40 -0700

    Fix bug where slideshow was not advancing every 5 seconds
    I called the function instead of passing function reference to
    setInterval.

--- src/index.js
+++ src/index.js
@@ -137,5 +137,5 @@ const toggleVisibility = function (element) {
   createNavButtons();
   bindNavButtons();     
   updateActiveNavButton(0);
-  setInterval(next(), 5000);
+  setInterval(next, 5000);
 })();
