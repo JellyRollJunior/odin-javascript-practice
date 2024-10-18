8d134d49eb96be5da8081b84b1c0e6d37b423089
Brandon Lin <brandonlin9716@gmail.com>
2024-10-18 00:21:56 -0700

    Display error message when email is invalid

--- src/form.js
+++ src/form.js
@@ -3,19 +3,27 @@ export { form };
 const form = (function form() {
     const form = document.querySelector('form');
     const email = document.querySelector('#email');
+    const emailError = document.querySelector('#email+.error');
     
     const emailConstraint = new RegExp('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,}$', '');
 
     const validateEmail = () => {
         const emailAddress = email.value;
-
         if (emailConstraint.test(emailAddress)) {
-            email.setCustomValidity("");
-        } else if (email.validity.typeMismatch) {
-            email.setCustomValidity("Please enter an email with format email@example.com")
-        } else if (email.validity.tooShort) {
-            email.setCustomValidity("Please enter an email with at least 5 characters")
+            emailError.textContent = '';
+            emailError.className = 'error';
+        } else {
+            showEmailError();
         }
+    }
+
+    const showEmailError = () => {
+        if (email.validity.typeMismatch || email.validity.valueMissing) {
+            emailError.textContent = 'Please enter an email with format mail@example.domain';
+        } else if (email.validity.tooShort) {
+            emailError.textContent = 'Please enter an email with at least 5 characters';
+        }
+        emailError.className = 'error active';
     }
 
     email.addEventListener('input', () => validateEmail());
