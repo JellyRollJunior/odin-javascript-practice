b8aee29d28a96c46dfef14ba043b1b1872a52e2a
Brandon Lin <brandonlin9716@gmail.com>
2024-10-18 12:03:59 -0700

    Add zipcode constraints
    european countries zipcode constraint code from
    https://developer.mozilla.org/en-US/docs/Web/HTML/Constraint_validation

--- src/form.js
+++ src/form.js
@@ -47,6 +47,32 @@ const country = (function country() {
     return { getCountryCode };
 })();
 
+const zipcode = (function zipcode() {
+    const zipcode = document.querySelector('#zipcode');
+    const constraints = {
+        tw: [
+            '^[\\d]{3}((-)?[\\d]{2,3})?$',
+            'Taiwanese postal codes must have 3, 5, or 6 digits: e.g. 11052 or 111-222',
+        ],
+        ch: [
+            '^(CH-)?\\d{4}$',
+            'Swiss postal codes must have exactly 4 digits: e.g. CH-1950 or 1950',
+        ],
+        fr: [
+            '^(F-)?\\d{5}$',
+            'French postal codes must have exactly 5 digits: e.g. F-75012 or 75012',
+        ],
+        de: [
+            '^(D-)?\\d{5}$',
+            'German postal codes must have exactly 5 digits: e.g. D-12345 or 12345',
+        ],
+        nl: [
+            '^(NL-)?\\d{4}\\s*([A-RT-Z][A-Z]|S[BCE-RT-Z])$',
+            'Dutch postal codes must have exactly 4 digits, followed by 2 letters except SA, SD and SS',
+        ],
+    };
+})();
+
 const form = (function form() {
     const form = document.querySelector('form');
     const output = document.querySelector('output');
