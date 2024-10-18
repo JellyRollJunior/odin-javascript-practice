037436b7c2289e4218cc5f6f88293365fbc2ac95
Brandon Lin <brandonlin9716@gmail.com>
2024-10-18 00:38:01 -0700

    Change javascript tabwidth to 4

--- src/form.js
+++ src/form.js
@@ -3,12 +3,15 @@ export { form };
 const email = (function email() {
     const email = document.querySelector('#email');
     const emailError = document.querySelector('#email+.error');
-    const emailConstraint = new RegExp('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,}$', '');
+    const emailConstraint = new RegExp(
+        '^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,}$',
+        ''
+    );
 
     const isEmailValid = () => {
         const emailAddress = email.value;
         return emailConstraint.test(emailAddress);
-    }
+    };
 
     const validateEmail = () => {
         if (isEmailValid()) {
@@ -17,32 +20,34 @@ const email = (function email() {
         } else {
             showEmailError();
         }
-    }
+    };
 
     const showEmailError = () => {
         if (email.validity.typeMismatch || email.validity.valueMissing) {
-            emailError.textContent = 'Please enter an email with format mail@example.domain';
+            emailError.textContent =
+                'Please enter an email with format mail@example.domain';
         } else if (email.validity.tooShort) {
-            emailError.textContent = 'Please enter an email with at least 5 characters';
+            emailError.textContent =
+                'Please enter an email with at least 5 characters';
         }
         emailError.className = 'error active';
-    }
+    };
 
     email.addEventListener('input', () => validateEmail());
 
-    return { isEmailValid, };
+    return { isEmailValid };
 })();
 
 const form = (function form() {
     const form = document.querySelector('form');
     const output = document.querySelector('output');
-    
+
     form.addEventListener('submit', (event) => {
         event.preventDefault();
         if (email.isEmailValid()) {
-            output.textContent = "YIPPEE - High five time!";
+            output.textContent = 'YIPPEE - High five time!';
         } else {
-            output.textContent = "Oops, check your fields again!";
+            output.textContent = 'Oops, check your fields again!';
         }
     });
 })();
--- src/index.js
+++ src/index.js
@@ -2,141 +2,141 @@ import './styles.css';
 import { form } from './form.js';
 
 const toggleVisibility = function (element) {
-  if (element.classList.contains('visible')) {
-    element.classList.remove('visible');
-  } else {
-    element.classList.add('visible');
-  }
+    if (element.classList.contains('visible')) {
+        element.classList.remove('visible');
+    } else {
+        element.classList.add('visible');
+    }
 };
 
 (function dropdownMenu() {
-  const dropdownMenuButton = document.querySelector('.dropdown-menu-btn');
-  dropdownMenuButton.addEventListener('click', () => {
-    const dropdownMenu = document.querySelector('.dropdown-menu');
-    toggleVisibility(dropdownMenu);
-  });
-
-  class button {
-    constructor(selector, action) {
-      this.selector = selector;
-      this.action = action;
-    }
-
-    bindEventListener() {
-      const button = document.querySelector(this.selector);
-      button.addEventListener('click', () => this.action());
-    }
-  }
-
-  const edit = () => {
-    console.log('You are editing!');
-  };
-
-  const copy = () => {
-    console.log('You are copying!');
-  };
-
-  const deleteAction = () => {
-    console.log('You are deleting!');
-  };
-
-  function bindDropdownMenuButtons(buttons) {
-    buttons.forEach((button) => {
-      button.bindEventListener();
+    const dropdownMenuButton = document.querySelector('.dropdown-menu-btn');
+    dropdownMenuButton.addEventListener('click', () => {
+        const dropdownMenu = document.querySelector('.dropdown-menu');
+        toggleVisibility(dropdownMenu);
     });
-  }
 
-  const dropdownMenuButtons = [
-    new button('.dropdown-menu > .edit-btn', edit),
-    new button('.dropdown-menu > .copy-btn', copy),
-    new button('.dropdown-menu > .delete-btn', deleteAction),
-  ];
+    class button {
+        constructor(selector, action) {
+            this.selector = selector;
+            this.action = action;
+        }
 
-  bindDropdownMenuButtons(dropdownMenuButtons);
+        bindEventListener() {
+            const button = document.querySelector(this.selector);
+            button.addEventListener('click', () => this.action());
+        }
+    }
+
+    const edit = () => {
+        console.log('You are editing!');
+    };
+
+    const copy = () => {
+        console.log('You are copying!');
+    };
+
+    const deleteAction = () => {
+        console.log('You are deleting!');
+    };
+
+    function bindDropdownMenuButtons(buttons) {
+        buttons.forEach((button) => {
+            button.bindEventListener();
+        });
+    }
+
+    const dropdownMenuButtons = [
+        new button('.dropdown-menu > .edit-btn', edit),
+        new button('.dropdown-menu > .copy-btn', copy),
+        new button('.dropdown-menu > .delete-btn', deleteAction),
+    ];
+
+    bindDropdownMenuButtons(dropdownMenuButtons);
 })();
 
 (function carousel() {
-  const images = document.querySelector('.images');
-  const navButtonHolder = document.querySelector('.nav-btns');
-  const right = document.querySelector('.right');
-  const left = document.querySelector('.left');
+    const images = document.querySelector('.images');
+    const navButtonHolder = document.querySelector('.nav-btns');
+    const right = document.querySelector('.right');
+    const left = document.querySelector('.left');
 
-  const numImages = images.children.length;
-  const imageLength = images.clientWidth / numImages;
-  let activeImageIndex = 0;
+    const numImages = images.children.length;
+    const imageLength = images.clientWidth / numImages;
+    let activeImageIndex = 0;
 
-  function setCarouselImage(index) {
-    const imageIndex = index % numImages;
-    const imageOffset = imageLength * imageIndex;
-    images.style.left = `-${imageOffset}px`;
-  }
-
-  function createNavButton(index) {
-    const navButton = document.createElement('button');
-    navButton.dataset.index = index;
-    return navButton;
-  }
-
-  function createNavButtons() {
-    for (let i = 0; i < numImages; i++) {
-      const navButton = createNavButton(i);
-      navButtonHolder.appendChild(navButton);
+    function setCarouselImage(index) {
+        const imageIndex = index % numImages;
+        const imageOffset = imageLength * imageIndex;
+        images.style.left = `-${imageOffset}px`;
     }
-  }
 
-  function clearActiveNavButton() {
-    const navButtons = document.querySelectorAll(`.nav-btns > *`);
-    navButtons.forEach((button) => {
-      button.classList.remove('active');
-    });
-  }
+    function createNavButton(index) {
+        const navButton = document.createElement('button');
+        navButton.dataset.index = index;
+        return navButton;
+    }
 
-  function setActiveNavButton(index) {
-    const navButton = document.querySelector(
-      `.nav-btns > :nth-child(${index + 1})`
-    );
-    navButton.classList.add('active');
-  }
+    function createNavButtons() {
+        for (let i = 0; i < numImages; i++) {
+            const navButton = createNavButton(i);
+            navButtonHolder.appendChild(navButton);
+        }
+    }
 
-  function updateActiveNavButton(index) {
-    clearActiveNavButton();
-    setActiveNavButton(index);
-  }
+    function clearActiveNavButton() {
+        const navButtons = document.querySelectorAll(`.nav-btns > *`);
+        navButtons.forEach((button) => {
+            button.classList.remove('active');
+        });
+    }
 
-  function handleClickNavButton(target) {
-    activeImageIndex = Number.parseInt(target.dataset.index);
-    setCarouselImage(activeImageIndex);
-    updateActiveNavButton(activeImageIndex);
-  }
+    function setActiveNavButton(index) {
+        const navButton = document.querySelector(
+            `.nav-btns > :nth-child(${index + 1})`
+        );
+        navButton.classList.add('active');
+    }
 
-  function bindNavButtons() {
-    const navButtons = document.querySelectorAll(`.nav-btns > *`);
-    navButtons.forEach((button) => {
-      button.addEventListener('click', (event) => {
-        const target = event.target;
-        handleClickNavButton(target);
-      });
-    });
-  }
+    function updateActiveNavButton(index) {
+        clearActiveNavButton();
+        setActiveNavButton(index);
+    }
 
-  function next() {
-    activeImageIndex = (activeImageIndex + 1) % numImages;
-    setCarouselImage(activeImageIndex);
-    updateActiveNavButton(activeImageIndex);
-  }
+    function handleClickNavButton(target) {
+        activeImageIndex = Number.parseInt(target.dataset.index);
+        setCarouselImage(activeImageIndex);
+        updateActiveNavButton(activeImageIndex);
+    }
 
-  function previous() {
-    // % is actually the remainder operator in javascript ... no modulo
-    activeImageIndex =
-      (((activeImageIndex - 1) % numImages) + numImages) % numImages;
-    setCarouselImage(activeImageIndex);
-    updateActiveNavButton(activeImageIndex)
-  }
+    function bindNavButtons() {
+        const navButtons = document.querySelectorAll(`.nav-btns > *`);
+        navButtons.forEach((button) => {
+            button.addEventListener('click', (event) => {
+                const target = event.target;
+                handleClickNavButton(target);
+            });
+        });
+    }
 
-  right.addEventListener('click', () => next());
-  left.addEventListener('click', () => previous());
-  createNavButtons();
-  bindNavButtons();     
-  updateActiveNavButton(0);
-  setInterval(next, 5000);
+    function next() {
+        activeImageIndex = (activeImageIndex + 1) % numImages;
+        setCarouselImage(activeImageIndex);
+        updateActiveNavButton(activeImageIndex);
+    }
+
+    function previous() {
+        // % is actually the remainder operator in javascript ... no modulo
+        activeImageIndex =
+            (((activeImageIndex - 1) % numImages) + numImages) % numImages;
+        setCarouselImage(activeImageIndex);
+        updateActiveNavButton(activeImageIndex);
+    }
+
+    right.addEventListener('click', () => next());
+    left.addEventListener('click', () => previous());
+    createNavButtons();
+    bindNavButtons();
+    updateActiveNavButton(0);
+    setInterval(next, 5000);
 })();
