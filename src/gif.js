b44f6877ec3e1e0bb17e8c08701ae1a11b970b47
Brandon Lin <brandonlin9716@gmail.com>
2024-10-21 23:49:24 -0700

    Display gifs based on input

--- src/gif.js
+++ src/gif.js
@@ -1,24 +1,31 @@
 export { initGif };
 
 const initGif = () => {
+    const img = document.querySelector('.gif > img');
+    const generateButton = document.querySelector('.gif > button');
     // todo: remove this
     const GIPHY_API_KEY = '';
-    const url = `https://api.giphy.com/v1/gifs/translate?api_key=${GIPHY_API_KEY}&s=cats`;
+    const url = `https://api.giphy.com/v1/gifs/translate?api_key=${GIPHY_API_KEY}`;
 
-    function populateGif() {
-        const img = document.querySelector('.gif > img');
-
-        fetch(url, { mode: 'cors' })
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (response) {
-            img.src = response.data.images.original.url;
-        });
+    function createEndpoint() {
+        let search = document.querySelector('#gif-search').value;
+        if (search === '') search = 'cats';
+        return `${url}&s=${search}`;
     }
 
-    const generateButton = document.querySelector('.gif > button');
+    function populateGif() {
+        const url = createEndpoint();
+        fetch(url, { mode: 'cors' })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (response) {
+                img.src = response.data.images.original.url;
+            });
+    }
+
     generateButton.addEventListener('click', () => populateGif());
+    populateGif();
 
     return { populateGif };
 };
