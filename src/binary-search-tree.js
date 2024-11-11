7fe86cc6ecadec9854a462b7ba0de031487f438b
Brandon Lin <brandonlin9716@gmail.com>
2024-11-10 22:16:37 -0800

    Implement build tree with function factories. Add pretty print tree
    pretty print code from:
    https://www.theodinproject.com/lessons/javascript-binary-search-trees

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,13 +1,45 @@
-class Node {
-    constructor(value, left = null, right = null) {
-        this.value = value;
-        this.left = left;
-        this.right = right;
-    }
+function node(data, left = null, right = null) {
+    return { data, left, right };
 }
 
-class Tree {
-    constructor(array) {
-        this.root = buildTree(array);
+function tree(array) {
+
+    const buildTree = (array) => {
+        array.sort();
+        // remove duplicates
+        const values = [...new Set(array)];
+        // construct tree
+        return treeBuilding(0, values.length - 1, values);
     }
+
+    const treeBuilding = (start, end, values) => {
+        if (start > end) return null;
+        let mid = Math.floor((start + end) / 2);
+        const current = node(
+            values[mid],
+            treeBuilding(start, mid - 1, values),
+            treeBuilding(mid + 1, end, values)
+        );
+        return current;
+    }
+
+    const prettyPrint = (node, prefix = "", isLeft = true) => {
+        if (node === null) {
+            return;
+        }
+        if (node.right !== null) {
+            prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+        }
+        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+        if (node.left !== null) {
+            prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+        }
+    };
+
+    let root = buildTree(array);
+
+    return {root, prettyPrint}
 }
+
+const test = tree([5, 4, 3]);
+test.prettyPrint(test.root);
