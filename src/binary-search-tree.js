29d2bd80f4dd2082a9420ce91ddb79dafd6e762e
Brandon Lin <brandonlin9716@gmail.com>
2024-11-12 10:22:02 -0800

    Implement inorder traversal

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -9,7 +9,7 @@ function tree(array) {
         const values = [...new Set(array)];
         // construct tree
         return buildTreeRecursive(0, values.length - 1, values);
-    }
+    };
 
     const buildTreeRecursive = (start, end, values) => {
         if (start > end) return null;
@@ -20,13 +20,13 @@ function tree(array) {
             buildTreeRecursive(mid + 1, end, values)
         );
         return current;
-    }
+    };
 
     const insert = (value) => {
         insertRecursive(value, root);
-    }
+    };
 
-    const insertRecursive = (value, current) =>  {
+    const insertRecursive = (value, current) => {
         if (current == null) return;
         if (value < current.data) {
             if (current.left == null) {
@@ -40,7 +40,7 @@ function tree(array) {
             insertRecursive(value, current.right);
         }
         return;
-    }
+    };
 
     const find = (value) => {
         let current = root;
@@ -52,10 +52,11 @@ function tree(array) {
             }
         }
         return current;
-    }
+    };
 
     const levelOrder = (callback) => {
-        if (typeof(callback) !== 'function') throw new Error('Parameter is not a callback function!');
+        if (typeof callback !== 'function')
+            throw new Error('Parameter is not a callback function!');
         const queue = [root];
         while (queue.length > 0) {
             let current = queue.shift();
@@ -63,7 +64,20 @@ function tree(array) {
             if (current.right != null) queue.push(current.right);
             callback(current);
         }
-    }
+    };
+
+    const inOrder = (callback) => {
+        if (typeof callback !== 'function')
+            throw new Error('Parameter is not a callback function!');
+        inOrderRecursive(callback, root);
+    };
+
+    const inOrderRecursive = (callback, node) => {
+        if (node == null) return;
+        if (node.left != null) inOrderRecursive(callback, node.left);
+        callback(node);
+        if (node.right != null) inOrderRecursive(callback, node.right);
+    };
 
     const depth = (value) => {
         if (value == root.data) return 0;
@@ -78,24 +92,36 @@ function tree(array) {
             depth += 1;
         }
         return current == null ? null : depth;
-    }
+    };
 
-    const prettyPrint = (node, prefix = "", isLeft = true) => {
+    const prettyPrint = (node, prefix = '', isLeft = true) => {
         if (node === null) {
             return;
         }
         if (node.right !== null) {
-            prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+            prettyPrint(
+                node.right,
+                `${prefix}${isLeft ? '│   ' : '    '}`,
+                false
+            );
         }
-        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+        console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
         if (node.left !== null) {
-            prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+            prettyPrint(
+                node.left,
+                `${prefix}${isLeft ? '    ' : '│   '}`,
+                true
+            );
         }
     };
 
     let root = buildTree(array);
 
-    return {root, insert, find, levelOrder, depth, prettyPrint}
+    return { root, insert, find, levelOrder, inOrder, depth, prettyPrint };
+}
+
+function call(node) {
+    console.log(node.data);
 }
 
 const test = tree([5, 4, 3]);
@@ -105,8 +131,5 @@ test.insert(5);
 test.insert(9);
 test.insert(8);
 test.prettyPrint(test.root);
-
-function call(node) {
-    console.log(node.data);
-}
-test.levelOrder(call);
+// test.levelOrder(call);
+// test.inOrder(call);
